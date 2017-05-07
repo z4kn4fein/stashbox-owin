06811010b6ec924031aa8aedd8e7465afcfcf16c
Peter Csajtai <peter.csajtai@logmein.com>
2017-05-07 21:33:55 +0200

    Added project

---
+++ src/stashbox.owin/OwinContextExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin;
+using Stashbox.Infrastructure;
+
+namespace Stashbox.Owin
+{
+    /// <summary>
+    /// Represents the stashbox related <see cref="IOwinContext"/> extension methods.
+    /// </summary>
+    public static class OwinContextExtensions
+    {
+        /// <summary>
+        /// Gets the current lifetime scope stored in the current <see cref="IOwinContext"/>.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <returns>The scope.</returns>
+        public static IDependencyResolver GetCurrentStashboxScope(this IOwinContext context) =>
+            context.Get<IDependencyResolver>(Constants.LifetimeScopeKey);
+    }
+}
---
+++ src/stashbox.owin/StashboxScopeMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Owin;
+using Stashbox.Infrastructure;
+
+namespace Stashbox.Owin
+{
+    internal class StashboxScopeMiddleware : OwinMiddleware
+    {
+        private readonly IStashboxContainer container;
+
+        public StashboxScopeMiddleware(OwinMiddleware next, IStashboxContainer container) : base(next)
+        {
+            this.container = container;
+        }
+
+        public override async Task Invoke(IOwinContext context)
+        {
+            using (var scope = this.container.BeginScope())
+            {
+                context.Set(Constants.LifetimeScopeKey, scope);
+                await base.Next.Invoke(context);
+            }
+        }
+    }
+}
